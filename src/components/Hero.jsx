d44e44f7bc9cbd193a52eb1f4fcd94d223b3d232
Mustafa Batuhan Büber <mustafa.buber@std.bogazici.edu.tr>
2025-10-25 11:21:32 +0300

    Test deployment

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,6 +3,7 @@ export default function Hero() {
       <section className="flex flex-col items-center justify-center h-screen text-center">
         <h1 className="text-5xl font-bold">Hi, I'm Batuhan Buber</h1>
         <p className="text-xl mt-4">Computer Engineer - Designer - Developer</p>
+        <p className="text-xl mt-4">Deployment Test</p>
       </section>
     );
   }
