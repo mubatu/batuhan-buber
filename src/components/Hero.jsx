ccc3f72d3e0926d149ae66bee7c46596f509206f
Mustafa Batuhan Büber <mustafa.buber@std.bogazici.edu.tr>
2025-10-25 13:45:41 +0300

    Add header

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 export default function Hero() {
     return (
       <section className="flex flex-col items-center justify-center h-screen text-center">
-        <h1 className="text-5xl font-bold">Hi, I'm Batuhan Buber</h1>
+        <h1 className="text-5xl font-bold">Batuhan Buber</h1>
         <p className="text-xl mt-4">Computer Engineer - Designer - Developer</p>
       </section>
     );
