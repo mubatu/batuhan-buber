ff1e701a25d5f873191f4f0daa16aae3e8569c86
Mustafa Batuhan Büber <mustafa.buber@std.bogazici.edu.tr>
2025-10-28 23:54:56 +0300

    Update About paragraph

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -50,7 +50,7 @@ export default function Hero() {
             About
           </h1>
           <p className="text-lg leading-relaxed mb-12" style={{ color: 'var(--color-text-primary)' }}>
-          I believe my greatest strengths are always asking questions, never doing things halfway, and having an eye for design.<br /><br />
+          I believe my greatest strengths are asking many questions, never doing things halfway, and having an eye for design.<br /><br />
           Outside of work, I love games, sports, movies, and photography.
           </p>
           <div className="flex flex-col gap-4">
